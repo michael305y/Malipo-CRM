d1b4dc790e3c1985e57f486e864610fa09417b54
Michael305y <antolando231@gmail.com>
2023-04-04 18:45:16 +0300

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,17 @@
+# imorting connectors of DB
+
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "1@bitch2"
+)
+
+#  creating cursor object
+cursorObject = database.cursor()
+
+# creating DB
+cursorObject. execute("CREATE DATABASE Malipo_db")
+
+print("DB created")
